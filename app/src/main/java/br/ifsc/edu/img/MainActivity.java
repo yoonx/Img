6352d1c90e0a36e9850fc2f064b7cc8a83f40646
Samantha <samanthagonzagalocatelli03@gmail.com>
2020-03-05 11:34:55 -0300

    Initial commit

---
+++ app/src/main/java/br/ifsc/edu/img/MainActivity.java
@@ -0,0 +1,26 @@
+package br.ifsc.edu.img;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+   ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public void mudaImagem (View view){
+        imageView.setImageResource(R.drawable.porquinho);
+    }
+}
+
+
+
+
---
+++ app/src/main/java/br/ifsc/edu/img/atvd2.java
@@ -0,0 +1,14 @@
+package br.ifsc.edu.img;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class atvd2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atvd2);
+    }
+}
